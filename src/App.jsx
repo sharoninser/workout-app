a5542b55601dcd78886b557c412466fcc6be9c24
Sergey <sharonin.dev@gmail.com>
2022-05-24 23:01:30 +0300

    создание репозитория

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './components/common/Layout'
+import Home from './components/pages/Home/Home'
+
+const App = () => {
+	return (
+		<Layout>
+			<Home />
+		</Layout>
+	)
+}
+
+export default App
